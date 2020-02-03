127b69666a72b0f65ed3bf8d589d9680fac9b0bf
the-kb <the-kb@ultronia2.0.2>
2020-02-03 15:43:18 +0200

    push

---
+++ my-app/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Greet from './Components/Greet';
+import Hi from './Components/Welcom'
+
+function App() {
+  return (
+    <div className="App">
+      <Greet />
+      <Hi />
+    </div>
+  );
+}
+
+export default App;
---
+++ my-app/src/Components/Greet.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+// function Greet() {
+//     return(<h1>dady is home</h1>);
+// }
+
+const Greet = ( ) => {
+   return(<h5>ruller</h5>);
+}
+
+export default Greet;
